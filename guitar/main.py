e9ac9c72f02d9ed99acf936d294acc7291a8bca4
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-15 00:43:42 +0900

    Add export chord book functions

--- guitar/main.py
+++ guitar/main.py
@@ -45,12 +45,17 @@ def find_notes_positions(notes):
     return notes_positions
 
 class Guitar():
-    def __init__(self, key="C", scale="major"):
+    def __init__(self, key="C", scale="major", dark_mode=False):
         self.key = key
         self.scale = scale
         self.intervals = SCALE2INTERVALS.get(scale.lower())
         self.notes = get_notes(key, self.intervals)
         self.notes_pos_in_string = find_notes_positions(self.notes)
+        self.facecoloer, self.linecolor = {
+            True  : ("black", "white"),
+            False : ("white", "black")
+        }[dark_mode]
+        self.chords = []
 
     @property
     def name(self):
@@ -64,30 +69,69 @@ class Guitar():
     def pdf(self):
         return self.name + ".pdf"
 
-    def _plot_chord_layout(self, dark_model=False):
-        facecoloer, linecolor = {
-            True  : ("black", "white"),
-            False : ("white", "black")
-        }[dark_model]
+    def chord_layout_create(self, n=1):
+        fig, axes = plt.subplots(ncols=1, nrows=n, figsize=(NUM_FRETS, NUM_STRINGS*n))
+        if n > 1:
+            for ax in axes:
+                ax = self.plot_chord_layout(ax=ax)
+        else:
+            axes = self.plot_chord_layout(ax=axes)
+        return fig, axes
 
-        fig,ax = plt.subplots(figsize=(NUM_FRETS, NUM_STRINGS))
+    def plot_chord_layout(self, ax=None):
+        if ax is None:
+            fig,ax = self.chord_layout_create(n=1)
         # Plot Strings
         for i in range(1, NUM_STRINGS+1):
             ax.plot([i for _ in range(NUM_FRETS+2)], color='gray')
         # Plot Frets
         for i in range(1, NUM_FRETS+1):
-            if i == LEN_OCTAVES:
+            if i%LEN_OCTAVES==0:
                 ax.axvline(x=i, color='gray', linewidth=3.5)
             else:
-                ax.axvline(x=i, color=linecolor, linewidth=0.5)
+                ax.axvline(x=i, color=self.linecolor, linewidth=0.5)
         ax.set_axisbelow(True)
-        ax.set_xlim([0.5, 21]), ax.set_ylim([0.4, 6.5])
-        ax.set_facecolor(facecoloer)
-        plt.xticks(np.arange(NUM_FRETS+1)+0.5, np.arange(NUM_FRETS+2), fontsize=20)
-        plt.yticks(np.arange(1,NUM_STRINGS+1), INIT_KEYS, fontsize=20)
-        return fig, ax
+        ax.set_facecolor(self.facecoloer)
+        ax.set_xlim([0.5, 21])
+        ax.set_xticks(np.arange(NUM_FRETS+1)+0.5)
+        ax.set_xticklabels(np.arange(NUM_FRETS+2), fontsize=20)
+        ax.set_ylim([0.4, 6.5])
+        ax.set_yticks(np.arange(1,NUM_STRINGS+1))
+        ax.set_yticklabels(INIT_KEYS, fontsize=20)
+        return ax
 
-    def plot_chord(self, chode, string=6, mode="major", dark_model=False):
+    def set_chord(self, chode, string=6, mode="major", set_title=True):
+        self.chords.append({
+            "chode"     : chode,
+            "string"    : string,
+            "mode"      : mode,
+            "set_title" : set_title
+        })
+
+    def export_chord_book(self, filename=None, fmt="pdf"):
+        num_chords = len(self.chords)
+        n_rows = num_chords//2+2 if num_chords%2 else num_chords//2+1
+        fig = plt.figure(figsize=(NUM_FRETS, NUM_STRINGS*n_rows))
+
+        ax_strings = plt.subplot2grid((n_rows, 2), (0, 0), colspan=2)
+        ax_strings = self.plot_chord_layout(ax=ax_strings)
+        ax_strings = self.plot_strings(ax=ax_strings)
+
+        for i,chords in enumerate(self.chords):
+            root_pos  = GUITAR_STRINGS.get(INIT_KEYS[6-chords.get("string")]).index(chords.get("chode"))
+
+            ax = plt.subplot2grid(shape=(n_rows, 2), loc=(1+i//2, i%2))
+            ax = self.plot_chord_layout(ax=ax)
+            ax = self.plot_chord(**chords, ax=ax)
+            ax.set_xlim([root_pos, min(root_pos+5, 21)])
+            ax.set_yticklabels([GUITAR_STRINGS.get(init_key)[root_pos-1] for init_key in INIT_KEYS], fontsize=20)
+
+        if fmt.lower() == "pdf":
+            fig.savefig(filename or self.pdf)
+        else:
+            fig.savefig(filename or self.png)
+
+    def plot_chord(self, chode, string=6, mode="major", set_title=True, ax=None):
         if chode not in self.notes:
             warnings.warn(f"{chode} is not included in the {self.notes}")
         elif mode[:5] in ["major", "minor"]:
@@ -101,7 +145,7 @@ class Guitar():
         is_mutes  = [pos==False for pos in positions]
         positions = [pos+root_pos for pos in positions]
 
-        fig, ax = self._plot_chord_layout(dark_model=dark_model)
+        ax = self.plot_chord_layout(ax)
         for i,(y_val, pos, is_mute) in enumerate(zip([1,2,3,4,5,6], positions, is_mutes)):
             x = pos+0.5
             note = GUITAR_STRINGS.get(INIT_KEYS[i])[pos]
@@ -114,11 +158,13 @@ class Guitar():
             ax.add_patch(mpatches.Circle(xy=(x, y_val), radius=radius, color=color))
             ax.annotate(note, (x, y_val), color='w', weight='bold',
                         fontsize=font, ha='center', va='center')
-        plt.title(self.name + f" [{chode}({string}s){mode}]", fontsize=20)
-        plt.show()
 
-    def plot_strings(self, dark_model=False, save=False):
-        fig, ax = self._plot_chord_layout(dark_model=dark_model)
+        if set_title:
+            plt.title(self.name + f" [{chode}({string}s){mode}]", fontsize=20)
+        return ax
+
+    def plot_strings(self, ax=None, set_title=True, width=20):
+        ax = self.plot_chord_layout(ax)
         for y_val, init_key in zip([1,2,3,4,5,6], INIT_KEYS):
             string = GUITAR_STRINGS.get(init_key)
             for i in self.notes_pos_in_string.get(init_key):
@@ -131,8 +177,6 @@ class Guitar():
                 ax.add_patch(mpatches.Circle(xy=(x, y_val), radius=radius, color=color))
                 ax.annotate(note, (x, y_val), color='w', weight='bold',
                             fontsize=font, ha='center', va='center')
-        plt.title(self.name, fontsize=20)
-        plt.show()
-        if save:
-            fig.savefig(self.png)
-            fig.savefig(self.pdf)
+        if set_title:
+            ax.set_title(self.name, fontsize=20)
+        return ax
