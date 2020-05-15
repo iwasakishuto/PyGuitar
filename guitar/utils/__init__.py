533ce864510bccc0a7b9a496af7b8bc25c2a7c48
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-15 19:14:03 +0900

    Utility modules

---
+++ guitar/utils/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+from . import generic_utils
+
+from .generic_utils import get_notes
+from .generic_utils import get_intervals
+from .generic_utils import find_notes_positions
+from .generic_utils import find_key_major_scale
