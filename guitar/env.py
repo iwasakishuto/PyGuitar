66bb42397636054773ae564cabdbcf059445186b
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-15 19:13:42 +0900

    Environment variables.

---
+++ guitar/env.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+import os
+import json
+
+__all__ = [
+    # "LIB_DIR_PATH", "REPO_DIR_PATH", "DATA_DIR_PATH",
+    "NUM_FRETS", "NUM_STRINGS", "LEN_OCTAVES", "INIT_KEYS",
+    "NOTES", "WHOLE_NOTES", "GUITAR_STRINGS",
+    "SCALE2INTERVALS", "CHORDS",
+]
+
+LIB_DIR_PATH  = os.path.dirname(os.path.abspath(__file__))
+REPO_DIR_PATH = os.path.dirname(LIB_DIR_PATH)
+DATA_DIR_PATH = os.path.join(REPO_DIR_PATH, "data")
+
+NUM_FRETS   = 20
+NUM_STRINGS = 6
+LEN_OCTAVES = 12
+INIT_KEYS   = ['E', 'A', 'D', 'G', 'B', 'E']
+NOTES       = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+WHOLE_NOTES = NOTES*3
+GUITAR_STRINGS = {
+    init_key : WHOLE_NOTES[WHOLE_NOTES.index(init_key):][:NUM_FRETS]
+    for init_key in INIT_KEYS
+}
+with open(os.path.join(DATA_DIR_PATH, "scales.json"), mode="r") as f:
+    SCALE2INTERVALS = json.load(f)
+with open(os.path.join(DATA_DIR_PATH, "chord.json"), mode="r") as f:
+    CHORDS = json.load(f)
