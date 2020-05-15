5ebdbadbeb38313fcb7b5c1b8e3a200cdd5b63c8
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-15 19:13:24 +0900

    Main guitar module.

--- guitar/main.py
+++ guitar/main.py
@@ -1,65 +1,13 @@
 # coding: utf-8
-import os
-import json
 import warnings
-from itertools import compress
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-from . import DATA_DIR_PATH
-
-NUM_FRETS   = 20
-NUM_STRINGS = 6
-LEN_OCTAVES = 12
-INIT_KEYS   = ['E', 'A', 'D', 'G', 'B', 'E']
-WHOLE_NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']*3
-GUITAR_STRINGS = {
-    init_key : WHOLE_NOTES[WHOLE_NOTES.index(init_key):][:NUM_FRETS]
-    for init_key in INIT_KEYS
-}
-with open(os.path.join(DATA_DIR_PATH, "scales.json"), mode="r") as f:
-    SCALE2INTERVALS = json.load(f)
-with open(os.path.join(DATA_DIR_PATH, "chord.json"), mode="r") as f:
-    CHORDS = json.load(f)
-
-def get_intervals(scale):
-    return SCALE2INTERVALS.get(scale.lower())
-
-def get_notes(key, intervals):
-    """
-    @params key       : Code key.
-    @params intervals : Scale positions.
-    @return notes     : Notes
-    """
-    if isinstance(intervals, str):
-        intervals = get_intervals(scale)
-    root = WHOLE_NOTES.index(key)
-    octave = WHOLE_NOTES[root:root+LEN_OCTAVES]
-    return [octave[i] for i in intervals]
-
-def find_notes_positions(notes):
-    notes_positions = dict()
-    for init_key, string in GUITAR_STRINGS.items():
-        indexes = []
-        for note in notes:
-            idx = string.index(note)
-            while idx < NUM_FRETS:
-                indexes.append(idx)
-                idx += LEN_OCTAVES
-        notes_positions[init_key] = indexes
-    return notes_positions
-
-def find_key_major_scale(majors=[], minors=[]):
-    is_majors = [1,0,0,1,1,0,0]
-    key_notes = {key: get_notes(key, SCALE2INTERVALS.get("major")) for key in WHOLE_NOTES[:LEN_OCTAVES]}
-    return {
-        k:v for k,v in key_notes.items()
-        if all([major in [e for e,is_major in zip(v,is_majors) if is_major] for major in majors])
-        and all([minor in [e for e,is_major in zip(v,is_majors) if not is_major] for minor in minors])
-    }
+from .env import *
+from .utils import get_notes, get_intervals, find_notes_positions
 
 class Guitar():
-    def __init__(self, key="C", scale="major", dark_mode=False):
+    def __init__(self, key="C", scale="major", dark_mode=False, name=""):
         self.key = key
         self.scale = scale
         self.intervals = get_intervals(scale)
@@ -70,6 +18,7 @@ class Guitar():
             False : ("white", "black")
         }[dark_mode]
         self.chords = []
+        self.name_ = name
 
     @property
     def name(self):
@@ -77,11 +26,13 @@ class Guitar():
 
     @property
     def png(self):
-        return self.name + ".png"
+        name = "" if self.name_ == "" else self.name_ + "-"
+        return name + self.name + ".png"
 
     @property
     def pdf(self):
-        return self.name + ".pdf"
+        name = "" if self.name_ == "" else self.name_ + "-"
+        return name + self.name + ".pdf"
 
     def chord_layout_create(self, n=1):
         fig, axes = plt.subplots(ncols=1, nrows=n, figsize=(NUM_FRETS, NUM_STRINGS*n))
@@ -174,7 +125,7 @@ class Guitar():
                         fontsize=font, ha='center', va='center')
 
         if set_title:
-            plt.title(self.name + f" [{chode}({string}s){mode}]", fontsize=20)
+            ax.set_title(self.name + f" [{chode}({string}s){mode}]", fontsize=20)
         return ax
 
     def plot_strings(self, ax=None, set_title=True, width=20):
