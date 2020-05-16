13adf631cb36e5ae521a9088ed3e684d43b5e751
iwasakishuto <Iwasaki-shuto0703@g.ecc.u-tokyo.ac.jp>
2020-05-16 18:06:17 +0900

    Handle chrome Driver.

---
+++ guitar/utils/driver_utils.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
+
+from .coloring_utils import *
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--ignore-certificate-errors')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+DRIVER_TYPE = "none"
+try:
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.quit()
+    DRIVER_TYPE = "remote"
+except:
+    pass
+
+try:
+    driver = webdriver.Remote(command_executor='http://selenium:4444/wd/hub',
+                              desired_capabilities=DesiredCapabilities.CHROME.copy(),
+                              options=chrome_options)
+    driver.quit()
+    DRIVER_TYPE = "local"
+except:
+    pass
+
+
+def driver_wrapper(func, *args, **kwargs):
+    if DRIVER_TYPE=="remote":
+        with webdriver.Remote(command_executor='http://selenium:4444/wd/hub',
+                              desired_capabilities=DesiredCapabilities.CHROME.copy(),
+                              options=chrome_options) as driver:
+            return func(driver, *args, **kwargs)
+    elif DRIVER_TYPE=="local":
+        with webdriver.Chrome(options=chrome_options) as driver:
+            return func(driver, *args, **kwargs)
+    else:
+        msg = "Could not create an instance of the 'chromedriver'. " + \
+        "If you can not prepare 'chromedriver' executable locally, " + \
+        "please build the environment with Dockerfile. Please see" + \
+        toBLUE("https://github.com/iwasakishuto/PyGuitar/tree/master/docker")
